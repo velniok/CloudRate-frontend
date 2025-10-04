298ae20d2c19ec422c91cce171770bd723f079a8
Ivan <mvzxw2@gmail.com>
2025-10-04 14:52:25 +0300

    init

--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: 'https://cloud-rate-backend.vercel.app',
 })
 
-console.log(process.env.REACT_APP_VERCEL_ENV)
+console.log(process.env.REACT_APP_API_URL)
 
 instance.interceptors.request.use((config) => {
     config.headers.Authorization = window.localStorage.getItem('token')
