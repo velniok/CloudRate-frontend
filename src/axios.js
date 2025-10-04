fd59f286d32168612adf1dd273a67a58374992b0
Ivan <mvzxw2@gmail.com>
2025-10-04 14:13:21 +0300

    init

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const instance = axios.create({
-    baseURL: `${import.meta.env.REACT_APP_API_URL}`,
+    baseURL: import.meta.env.REACT_APP_API_URL,
 })
 
 instance.interceptors.request.use((config) => {
