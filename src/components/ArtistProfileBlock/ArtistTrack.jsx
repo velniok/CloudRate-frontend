8c6d0540823477539fa4a86e2cdd2cac6bdc673b
Ivan <mvzxw2@gmail.com>
2025-10-15 16:52:28 +0300

    mobile artist page

--- src/components/ArtistProfileBlock/ArtistTrack.jsx
+++ src/components/ArtistProfileBlock/ArtistTrack.jsx
@@ -5,13 +5,15 @@ export default function ArtistTrack({ tracks }) {
     return (
         <div className="artist__track">
             <h2 className="artist__track-title title">Треки артиста</h2>
-            <ul className="artist__track-list">
-                {
-                    tracks.map(e => (
-                        <ArtistTrackItem key={e._id} name={e.name} avatarUrl={e.avatarUrl} id={e._id} artist={e.artist} ratingTrackOverall={e.ratingTrack[0].overall} />
-                    ))  
-                }                       
-            </ul>
+            <div className="artist__track-content">
+                <ul className="artist__track-list">
+                    {
+                        tracks.map(e => (
+                            <ArtistTrackItem key={e._id} name={e.name} avatarUrl={e.avatarUrl} id={e._id} artist={e.artist} ratingTrackOverall={e.ratingTrack[0].overall} />
+                        ))  
+                    }                       
+                </ul>
+            </div>
         </div>
     )
 }
